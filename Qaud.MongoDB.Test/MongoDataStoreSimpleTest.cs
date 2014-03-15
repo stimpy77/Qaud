1b43669cd022cdaacfe90e80821ad3135db4c285
stimpy77 <jon@jondavis.net>
2014-03-15 02:44:18 -0700

    Begin building a MongoDB implementation

---
+++ Qaud.MongoDB.Test/MongoDataStoreSimpleTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+using Qaud.Test;
+
+namespace Qaud.MongoDB.Test
+{
+    [TestClass]
+    public class MongoDataStoreSimpleTest : DataStoreSimpleTest
+    {
+        public MongoDataStoreSimpleTest() : base(CreateMongoDataStore())
+        {
+        }
+
+        private static MongoDbDataStore<FooModel> CreateMongoDataStore()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        protected override void AddItemToStore(FooModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void CleanOutItemFromStore(FooModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override FooModel GetItemById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void MongoDataStore_Add_Item_Adds_Item()
+        {
+            base.DataStore_Add_Item_Adds_Item();
+        }
+
+        [TestMethod]
+        public void MongoDataStore_Create_Instantiates_T()
+        {
+            base.DataStore_Create_Instantiates_T();
+        }
+
+        [TestMethod]
+        public void MongoDataStore_DeleteByKey_Removes_Item()
+        {
+            base.DataStore_DeleteByKey_Removes_Item();
+        }
+
+        [TestMethod]
+        public void MongoDataStore_Delete_Item_Range_Removes_Many_Items()
+        {
+            base.DataStore_Delete_Item_Range_Removes_Many_Items();
+        }
+
+        [TestMethod]
+        public void MongoDataStore_Delete_Item_Range_Single_Removes_Item()
+        {
+            base.DataStore_Delete_Item_Range_Single_Removes_Item();
+        }
+
+        [TestMethod]
+        public void MongoDataStore_Delete_Item_Removes_Item()
+        {
+            base.DataStore_Delete_Item_Removes_Item();
+        }
+
+        [TestMethod]
+        public void MongoDataStore_Partial_Update_Modifies_Item()
+        {
+            base.DataStore_Partial_Update_Modifies_Item();
+        }
+
+        [TestMethod]
+        public void MongoDataStore_Query_For_All_Returns_All()
+        {
+            base.DataStore_Query_For_All_Returns_All();
+        }
+
+        [TestMethod]
+        public void MongoDataStore_Query_For_Item_Returns_Result()
+        {
+            base.DataStore_Query_For_Item_Returns_Result();
+        }
+
+        [TestMethod]
+        public void MongoDataStore_Update_Modifies_Item()
+        {
+            base.DataStore_Update_Modifies_Item();
+        }
+    }
+}
---
+++ Qaud.MongoDB/MongoDbDataStore.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace Qaud.MongoDB
+{
+    public class MongoDbDataStore<T> : IDataStore<T> 
+    {
+        private MongoDatabase _db;
+        private MongoCollection<T> _collection;
+
+        public MongoDbDataStore(MongoDatabase db)
+            : this(db, ResolveCollectionName(typeof(T)))
+        {
+        }
+
+        public MongoDbDataStore(MongoDatabase db, string collectionName)
+        {
+            _db = db;
+            _collection = db.GetCollection<T>(collectionName);
+        }
+
+        public T Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(params object[] key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(params object[] key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindMatch(T lookup)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T item, out T result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRange(IEnumerable<T> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T UpdatePartial(object item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(IEnumerable<T> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Query
+        {
+            get { return _collection.AsQueryable(); }
+        }
+
+        bool IDataStore<T>.AutoSave
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IDataStore<T>.CanQueueChanges
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool IDataStore<T>.SupportsNestedRelationships
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool IDataStore<T>.SupportsComplexStructures
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool IDataStore<T>.SupportsTransactionScope
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool IDataStore<T>.SupportsGeneratedKeys
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        object IDataStore<T>.DataSet
+        {
+            get { return _collection; }
+        }
+
+        object IDataStore<T>.DataContext
+        {
+            get { return _db; }
+        }
+        private static string ResolveCollectionName(Type type)
+        {
+            // todo: use any clues from attributes or fluent descriptors that might define the collection name
+            return type.Name.Substring(0, 1).ToLower() + type.Name.Substring(1);
+        }
+    }
+}

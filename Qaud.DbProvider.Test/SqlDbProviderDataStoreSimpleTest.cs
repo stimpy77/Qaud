e64b6442060ca27f58b6725f5aea78390f03a24c
stimpy77 <jon@jondavis.net>
2014-03-16 20:57:07 -0700

    starting to implement for DbProviderFactory (no tests pass)

---
+++ Qaud.DbProvider.Test/SqlDbProviderDataStoreSimpleTest.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Qaud.Test;
+
+namespace Qaud.DbProvider.Test
+{
+    [TestClass]
+    public class SqlDbProviderDataStoreSimpleTest : DataStoreSimpleTest
+    {
+        private const string TEST_DATABASE_NAME = "QAUD_TEST";
+        private const string TEST_CONNECTION_STRING = "Data Source=(localdb)\\v11.0;Integrated Security=true;Database=" + TEST_DATABASE_NAME + ";";
+        public SqlDbProviderDataStoreSimpleTest()
+            : base(new DbProviderDataStore<FooModel>(DbProviderFactories.GetFactory("System.Data.SqlClient"), TEST_CONNECTION_STRING))
+        {
+            RecreateTable();
+        }
+
+        private SqlConnection NewConnection(bool autoOpen = true)
+        {
+            var conn = new SqlConnection(TEST_CONNECTION_STRING);
+            if (autoOpen) conn.Open();
+            return conn;
+        }
+
+        protected virtual void RecreateTable()
+        {
+            var cmdCreateDatabase = "CREATE DATABASE QAUD_TEST";
+            var cmdDrop = @"DROP TABLE FooModel";
+
+            var cmdCreate = @"CREATE TABLE dbo.FooModel
+	                            (
+	                            ID bigint NOT NULL,
+	                            CreateDate datetime NULL,
+	                            Title nvarchar(500) NULL,
+	                            [Content] nvarchar(MAX) NULL
+	                            )  ON [PRIMARY]
+	                             TEXTIMAGE_ON [PRIMARY];
+                            
+                            ALTER TABLE dbo.FooModel ADD CONSTRAINT
+	                            PK_FooModel PRIMARY KEY CLUSTERED 
+	                            (
+	                            ID
+	                            ) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+                            ";
+            try
+            {
+                var newdb = NewConnection(false);
+                newdb.ConnectionString = TEST_CONNECTION_STRING.Substring(0, TEST_CONNECTION_STRING.IndexOf("Database="));
+                newdb.Open();
+                var cmd = new SqlCommand(cmdCreateDatabase, newdb);
+                cmd.ExecuteNonQuery();
+            }
+            catch {}
+            try
+            {
+                var cmd2 = new SqlCommand(cmdDrop, NewConnection());
+                cmd2.ExecuteNonQuery();
+            }
+            catch { }
+            var cmd3 = new SqlCommand(cmdCreate, NewConnection());
+            cmd3.ExecuteNonQuery();
+        }
+
+        protected override void AddItemToStore(FooModel item)
+        {
+            var conn = NewConnection();
+            var sql = @"INSERT INTO FooModel
+                        (
+                            ID, 
+                            CreateDate,
+                            Title,
+                            Content
+                        ) VALUES (
+                            @ID,
+                            @CreateDate,
+                            @Title,
+                            @Content
+                        )";
+            var cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.Add("@ID", item.ID);
+            cmd.Parameters.Add("@CreateDate", item.CreateDate);
+            cmd.Parameters.Add("@Title", item.Title);
+            cmd.Parameters.Add("@Content", item.Content);
+        }
+
+        protected override void CleanOutItemFromStore(FooModel item)
+        {
+            using (var conn = NewConnection())
+            {
+                var cmdtext = "DELETE FROM FooModel WHERE ID = @ID";
+                var cmd = new SqlCommand(cmdtext, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        protected override FooModel GetItemById(long id)
+        {
+            using (var conn = NewConnection())
+            {
+                var cmdtext = "SELECT * FROM FooModel WHERE ID = @ID";
+                var cmd = new SqlCommand(cmdtext, conn);
+                var dr = cmd.ExecuteReader();
+                var hydrator = new EntityMemberResolver<FooModel>();
+                dr.Read();
+                var item = new FooModel();
+                hydrator.HydrateFromDictionary(item,
+                    hydrator.ConvertToDictionary(dr));
+                return item;
+            }
+        }
+
+        [TestMethod]
+        public void SqlDbProviderDataStore_Add_Item_Adds_Item()
+        {
+            base.DataStore_Add_Item_Adds_Item();
+        }
+
+        [TestMethod]
+        public void SqlDbProviderDataStore_Create_Instantiates_T()
+        {
+            base.DataStore_Create_Instantiates_T();
+        }
+
+        [TestMethod]
+        public void SqlDbProviderDataStore_DeleteByKey_Removes_Item()
+        {
+            base.DataStore_DeleteByKey_Removes_Item();
+        }
+
+        [TestMethod]
+        public void SqlDbProviderDataStore_Delete_Item_Range_Removes_Many_Items()
+        {
+            base.DataStore_Delete_Item_Range_Removes_Many_Items();
+        }
+
+        [TestMethod]
+        public void SqlDbProviderDataStore_Delete_Item_Range_Single_Removes_Item()
+        {
+            base.DataStore_Delete_Item_Range_Single_Removes_Item();
+        }
+
+        [TestMethod]
+        public void SqlDbProviderDataStore_Delete_Item_Removes_Item()
+        {
+            base.DataStore_Delete_Item_Removes_Item();
+        }
+
+        [TestMethod]
+        public void SqlDbProviderDataStore_Partial_Update_Modifies_Item()
+        {
+            base.DataStore_Partial_Update_Modifies_Item();
+        }
+
+        [TestMethod]
+        public void SqlDbProviderDataStore_Query_For_All_Returns_All()
+        {
+            base.DataStore_Query_For_All_Returns_All();
+        }
+
+        [TestMethod]
+        public void SqlDbProviderDataStore_Query_For_Item_Returns_Result()
+        {
+            base.DataStore_Query_For_Item_Returns_Result();
+        }
+
+        [TestMethod]
+        public void SqlDbProviderDataStore_Update_Modifies_Item()
+        {
+            base.DataStore_Update_Modifies_Item();
+        }
+    }
+}
---
+++ Qaud.DbProvider/DbProviderDataStore.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qaud.DbProvider
+{
+    public class DbProviderDataStore<T> : IDataStore<T>, IDisposable
+    {
+
+        private DbConnection _connection;
+        private DbProviderFactory _providerFactory;
+        private EntityMemberResolver<T> _memberResolver;
+        private string _connName;
+
+        public DbProviderDataStore(DbProviderFactory providerFactory, string connectionName)
+        {
+            _providerFactory = providerFactory;
+            _connection = _providerFactory.CreateConnection();
+            _memberResolver = new EntityMemberResolver<T>();
+            _connName = connectionName;
+        }
+
+        private void OpenIfClosed()
+        {
+            if (string.IsNullOrEmpty(_connection.ConnectionString))
+            {
+                var connStringConfig = ConfigurationManager.ConnectionStrings[_connName];
+                _connection.ConnectionString = connStringConfig == null 
+                    ? _connName 
+                    : connStringConfig.ConnectionString;
+            }
+            if (_connection.State == ConnectionState.Broken) 
+                _connection.Close();
+            if (_connection.State == ConnectionState.Closed)
+            {
+                try
+                {
+                    _connection.Open();
+                }
+                catch
+                {
+                    var connstring = _connection.ConnectionString;
+                    _connection = _providerFactory.CreateConnection();
+                    _connection.ConnectionString = connstring;
+                    _connection.Open();
+                }
+            }
+        }
+
+        public virtual T Create()
+        {
+            return Activator.CreateInstance<T>();
+        }
+
+        public virtual void Add(T item)
+        {
+            var cmdbuilder = _providerFactory.CreateCommandBuilder();
+            var insertcmd = cmdbuilder.GetInsertCommand(true);
+            var dic = _memberResolver.ConvertToDictionary(item);
+            foreach (var kvp in dic)
+            {
+                var t = kvp.Value.GetType();
+                if (!IsComplexType(t))
+                {
+                    insertcmd.Parameters.Cast<DbParameter>()
+                        .First(
+                            p =>
+                                p.ParameterName.ToLower() == kvp.Key.ToLower() ||
+                                p.ParameterName.ToLower() == "@" + kvp.Key.ToLower())
+                        .Value = kvp.Value;
+                }
+            }
+            insertcmd.Connection = _connection;
+            OpenIfClosed();
+            insertcmd.Prepare();
+            insertcmd.ExecuteNonQuery();
+        }
+
+        private bool IsComplexType(Type t)
+        {
+            return t.IsPrimitive || t == typeof (DateTime) || t == typeof (DateTimeOffset);
+        }
+
+        public virtual void Add(T item, out T result)
+        {
+            Add(item);
+            result = item; // keygen tracking not yet supported, though it might be feasible
+        }
+
+        public virtual void AddRange(IEnumerable<T> items)
+        {
+            foreach (var item in items) Add(item);
+        }
+
+        public virtual T Find(params object[] key)
+        {
+            if (key == null) key = new object[] {};
+            var cmdbuilder = _providerFactory.CreateCommandBuilder();
+            var selectCmd = cmdbuilder.DataAdapter.SelectCommand;
+            var keymembers = _memberResolver.KeyPropertyMembers.ToArray();
+            if (keymembers.Length != key.Length)
+            {
+                throw new ArgumentException("Key component count mismatch. Should match [Key] attribute count.", "key");
+            }
+            for (var i=0; i<key.Length; i++)
+            {
+                var parameter = selectCmd.CreateParameter();
+                parameter.ParameterName = keymembers[i].Name;
+                parameter.Value = key[i];
+            }
+            OpenIfClosed();
+            selectCmd.Prepare();
+            using (var result = selectCmd.ExecuteReader())
+            {
+                if (result.HasRows && result.Read())
+                {
+                    var dictionary = _memberResolver.ConvertToDictionary(result);
+                    var item = Create();
+                    _memberResolver.HydrateFromDictionary(item, dictionary);
+                    return item;
+                }
+                return default(T);
+            }
+        }
+
+        public virtual T Find(T lookup)
+        {
+            return Find(_memberResolver.GetKeyPropertyValues(lookup));
+        }
+
+        public virtual void Update(T item)
+        {
+            var cmdbuilder = _providerFactory.CreateCommandBuilder();
+            var updatecmd = cmdbuilder.GetUpdateCommand(true);
+            var dic = _memberResolver.ConvertToDictionary(item);
+            foreach (var kvp in dic)
+            {
+                updatecmd.Parameters.Cast<DbParameter>()
+                    .First(
+                        p =>
+                            p.ParameterName.ToLower() == kvp.Key.ToLower() ||
+                            p.ParameterName.ToLower() == "@" + kvp.Key.ToLower())
+                    .Value = kvp.Value;
+            }
+            updatecmd.Prepare();
+            updatecmd.ExecuteNonQuery();
+        }
+
+        public virtual void UpdateRange(IEnumerable<T> items)
+        {
+            foreach (var item in items) Update(item);
+        }
+
+        public virtual T UpdatePartial(object item)
+        {
+            var key = _memberResolver.GetKeyPropertyValues(item);
+            var orig = Find(key);
+            _memberResolver.ApplyPartial(orig, item);
+            Update(orig);
+            return orig;
+        }
+
+        public virtual void Delete(params object[] key)
+        {
+            if (key == null) key = new object[] { };
+            var cmdbuilder = _providerFactory.CreateCommandBuilder();
+            var deleteCmd = cmdbuilder.GetDeleteCommand(true);
+            var keymembers = _memberResolver.KeyPropertyMembers.ToArray();
+            if (keymembers.Length != key.Length)
+            {
+                throw new ArgumentException("Key component count mismatch. Should match [Key] attribute count.", "key");
+            }
+            for (var i = 0; i < key.Length; i++)
+            {
+                var parameter = deleteCmd.CreateParameter();
+                parameter.ParameterName = keymembers[i].Name;
+                parameter.Value = key[i];
+            }
+            deleteCmd.Prepare();
+            deleteCmd.ExecuteNonQuery();
+        }
+
+        public virtual void DeleteItem(T item)
+        {
+            Delete(_memberResolver.GetKeyPropertyValues(item));
+        }
+
+        public virtual void DeleteRange(IEnumerable<T> items)
+        {
+            foreach (var item in items) Delete(item);
+        }
+
+        protected virtual List<T> GetAll()
+        {
+
+            var cmdbuilder = _providerFactory.CreateCommandBuilder();
+            var selectCmd = cmdbuilder.DataAdapter.SelectCommand;
+            selectCmd.Connection = _connection;
+            OpenIfClosed();
+            selectCmd.Prepare();
+            var reader = selectCmd.ExecuteReader();
+            var items = new List<T>();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    var item = Create();
+                    var row = _memberResolver.ConvertToDictionary(reader);
+                    _memberResolver.HydrateFromDictionary(item, row);
+                }
+            }
+            return items;
+        }
+
+
+        /// <summary>
+        /// Client-side filter with LINQ-to-Objects unless overridden
+        /// </summary>
+        protected virtual IQueryable<T> Query
+        {
+            get
+            {
+                return GetAll().AsQueryable();
+                //throw new NotImplementedException();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Query.GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return Query.GetEnumerator();
+        }
+
+        Expression IQueryable.Expression
+        {
+            get { return Query.Expression; }
+        }
+
+        Type IQueryable.ElementType
+        {
+            get { return Query.ElementType; }
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return Query.Provider; }
+        }
+
+        bool IDataStore<T>.AutoSave
+        {
+            get { return true; }
+            set { throw new NotImplementedException(); }
+        }
+
+        void IDataStore<T>.SaveChanges()
+        {
+            // no-op
+        }
+
+        bool IDataStore<T>.CanQueueChanges
+        {
+            get { return false; }
+        }
+
+        bool IDataStore<T>.SupportsNestedRelationships
+        {
+            get { return false; }
+        }
+
+        bool IDataStore<T>.SupportsComplexStructures
+        {
+            get { return false; }
+        }
+
+        bool IDataStore<T>.SupportsTransactionScope
+        {
+            get { return true; }
+        }
+
+        bool IDataStore<T>.SupportsGeneratedKeys
+        {
+            get { return false; }
+        }
+
+        object IDataStore<T>.DataSet
+        {
+            get { return null; }
+        }
+
+        object IDataStore<T>.DataContext
+        {
+            get { return _providerFactory; }
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+    }
+}
